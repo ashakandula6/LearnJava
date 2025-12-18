7bf609799448acb1d92f7c9d81ace6d918d748b3
Asha <ashapkandula@gmail.com>
2025-12-18 16:28:46 +0530

    Initial commit

---
+++ javaElse.java
@@ -0,0 +1,11 @@
+public class javaElse {
+  public static void main(String[] args) {
+    boolean isRaining = false;
+
+    if (isRaining) {
+      System.out.println("Bring an umbrella!");
+    } else {
+      System.out.println("No need for an umbrella!");
+    }
+  }
+}
